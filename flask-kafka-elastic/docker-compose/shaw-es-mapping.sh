fc9cc9a85b161a1cd87f5a9af7fb6adbb3b70913
Jordan Moore <crikket.007@gmail.com>
2020-01-21 15:36:54 -0600

    fix mapping file

--- flask-kafka-elastic/docker-compose/shaw-es-mapping.sh
+++ flask-kafka-elastic/docker-compose/shaw-es-mapping.sh
@@ -4,7 +4,7 @@ set -xv
 set -e
 
 ES_ADDR=${ES_ADDR:-"localhost:9200"}
-ES_INDEX=${ES_INDEX:-"kafkaconnect"}
+ES_INDEX=${ES_INDEX:-"shaw-products"}
 
 curl -XDELETE "http://${ES_ADDR}/${ES_INDEX}"
 curl -XPUT "http://${ES_ADDR}/${ES_INDEX}" -H 'Content-Type: application/json' -d'
